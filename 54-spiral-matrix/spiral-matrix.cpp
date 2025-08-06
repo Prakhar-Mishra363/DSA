a35c1bdbe8396e2d40c3e7f136be504272049434
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-08-06 20:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.20%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -2,46 +2,40 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int top = 0, bottom = matrix.size()-1,right=matrix[0].size()-1,left=0;
-        int totalEle=(bottom+1)*(right+1);
-        int count = 0;
+        // int totalEle=(bottom+1)*(right+1);
+        // int count = 0;
         vector<int> ansMat;
         while((left<=right)&&(top<=bottom)){
-            if(count!=totalEle){
+            
                 for(int i=top;i<=right;i++){
                     ansMat.push_back(matrix[top][i]);
-                    count++;
                 }
-                top++;
-            }else{
-                break;
-            }
-            if(count!=totalEle){
-                for(int i=top;i<=bottom;i++){
+            
+           
+                for(int i=top+1;i<=bottom;i++){
                     ansMat.push_back(matrix[i][right]);
-                    count++;
                 }
-                right--;
-            }else{
-                break;
-            }
-            if(count!=totalEle){
-                for(int i=right;i>=left;i--){
+            
+                for(int i=right-1;i>=left;i--){
+                    if(top==bottom){
+                        break;
+                    }
                     ansMat.push_back(matrix[bottom][i]);
-                    count++;
+                }
+            
+            
+           
+                for(int i=bottom-1;i>=top+1;i--){
+                    if(right==left){
+                        break;
+                    }
+                    ansMat.push_back(matrix[i][left]);
                 }
                 bottom--;
-            }else{
-                break;
-            }
-            if(count!=totalEle){
-                for(int i=bottom;i>=top;i--){
-                    ansMat.push_back(matrix[i][left]);
-                    count++;
-                }
+                top++;
+                right--;
                 left++;
-            }else{
-                break;
-            }
+            
         }
         
         return ansMat;
