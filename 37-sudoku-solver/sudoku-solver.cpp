a23c5c4910f432a1dd6b662752e063009f12aa9c
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-08-12 22:06:23 +0530

    Time: 285 ms (69.96%) | Memory: 8.8 MB (26.75%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -3,30 +3,12 @@ public:
     bool isSafe(vector<vector<char>>& board,int row, int col,char ch){
         for(int i=0;i<9;i++){
             if(board[row][i]==ch)return false;
-        }
-        //checks for the column
-        for(int i=0;i<9;i++){
             if(board[i][col]==ch)return false;
         }
-        int i;
-        int x=row/3,y=col/3;
-        if(x==0){
-            x=0;
-        }else if(x==1){
-            x=3;
-        }else{
-            x=6;
-        }
-        if(y==0){
-            y=0;
-        }else if(y==1){
-            y=3;
-        }else{
-            y=6;
-        }
+        int x=(row/3)*3,y=(col/3)*3;
         for(int i=x;i<(x+3);i++){
             for(int j=y;j<(y+3);j++){
-                if(board[i][j]==ch)return false;
+                    if(board[i][j]==ch)return false;
             }
         }
         return true;
@@ -40,10 +22,8 @@ public:
         for(char i='1';i<='9';i++){
             if(isSafe(board,row,col,(i))&&board[row][col]=='.'){
                 board[row][col]=(i);
-                // i++;
                 bool ans=sol(board,row,col+1);
                 if(ans)return true;
-                // i--;
                 board[row][col]='.';
             }
             
