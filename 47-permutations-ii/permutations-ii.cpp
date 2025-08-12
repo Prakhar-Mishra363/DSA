8ee3f76dba396d1c4b59d26496711e1d364179e2
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-08-12 18:40:34 +0530

    Time: 3 ms (67.17%) | Memory: 15.2 MB (15.38%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-void calPer(vector<int>& nums,vector<vector<int>>& ans,set<vector<int>>& s,int idx){
-    if(idx==nums.size()&&(s.find(nums)==s.end())){
+void calPer(vector<int>& nums,vector<vector<int>>& ans,int idx){
+    if(idx==nums.size()){
         ans.push_back({nums});
-        s.insert(nums);
         return;
     }
+    unordered_set<int>s;
     for(int i=idx;i<nums.size();i++){
+        if(s.count(nums[i])) continue;
+        s.insert(nums[i]);
         swap(nums[i],nums[idx]);
-        calPer(nums,ans,s,idx+1);
+        calPer(nums,ans,idx+1);
         swap(nums[i],nums[idx]);
     }
 }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
        vector<vector<int>>ans;
-       set<vector<int>>s;
-       calPer(nums,ans,s,0); 
+    //    set<vector<int>>s;
+       calPer(nums,ans,0); 
         return ans;
     }
 };
