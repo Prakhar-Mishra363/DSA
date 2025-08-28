17b6e7b8727f4b93119f4cf2550d06adfcb23ba4
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-08-28 19:15:44 +0530

    Time: 12 ms (53.11%) | Memory: 15.2 MB (8.83%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -5,7 +5,9 @@ public:
         queue<pair<char,int>>q;
         for(int i=0;i<s.length();i++){
             m[s[i]]++;
-            q.push({s[i],i});
+            if(m[s[i]]==1){
+                q.push({s[i],i});
+            }
         }
         while(!q.empty()){
             if(m[q.front().first]==1)return q.front().second;
