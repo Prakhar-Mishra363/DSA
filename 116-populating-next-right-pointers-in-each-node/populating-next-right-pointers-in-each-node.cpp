89b23c94d7dc7802835eda08af781670749ba333
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-09-07 09:31:49 +0530

    Time: 4 ms (98.42%) | Memory: 19.2 MB (44.54%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)return root;
+        queue<Node*>q;
+        if(root->left && root->right){
+            q.push(root->left);
+            q.push(root->right);
+        }else return root;
+        while(!q.empty()){
+            int currSize = q.size();
+            Node* curr = NULL;
+            for(int i=0;i<currSize-1;i++){
+               curr = q.front();
+               q.pop();
+               curr->next = q.front(); 
+               if(curr->left)q.push(curr->left);
+               if(curr->right)q.push(curr->right);
+            }
+            curr = q.front();
+            q.pop();
+            if(curr->left)q.push(curr->left);
+            if(curr->right)q.push(curr->right); 
+        }
+       return root; 
+    }
+};
