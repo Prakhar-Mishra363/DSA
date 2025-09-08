eca259b72efc34358a3f86bf216560c3d0b67bfd
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-09-08 18:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (14.09%) - LeetSync

---
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        if(n<10)return -1;
+        vector<int>store;
+        int num = n;
+        while(num>0){
+            store.push_back(num%10);
+            num/=10;
+        }
+        reverse(store.begin(),store.end());
+        int pivot = -1,end=store.size()-1;//find the element which is not in decreaseing order from the back
+        for(int i=end-1;i>=0;i--){
+            if(store[i]<store[i+1]){
+                pivot = i;break;
+            }
+        }
+        long ans = 0;
+        if(pivot==-1){
+            reverse(store.begin(),store.end());
+            for(int i=store.size()-1;i>=0;i--){
+                ans = ans*10+store[i];
+            }
+            if(ans>INT_MAX || ans<=n)return -1;
+            return ans;
+        }
+        for(int i=end;i>=0;i--){
+            if(store[i]>store[pivot]){
+                swap(store[i],store[pivot]);
+                break;
+            }
+        }
+        for(int i=pivot+1,j=end;i<j;){
+            swap(store[i++],store[j--]);
+        }
+        for(int i=0;i<store.size();i++){
+            ans = ans*10+store[i];
+        }
+        if(ans>INT_MAX || ans<=n)return -1;
+        return ans;
+    }
+};
