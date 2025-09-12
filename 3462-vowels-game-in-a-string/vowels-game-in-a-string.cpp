968ef48839fbc69e5de3e4464bb062e5dfca2129
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-09-12 19:01:37 +0530

    Time: 17 ms (12.69%) | Memory: 19.7 MB (5.97%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,33 +1,12 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        bool ans = false;
-        int turn=1,i=0;
-        unordered_set<char>ch={'a','e','i','o','u'};
-        while(i<s.length()){
-            int count=0;
-            for(;i<s.length();i++){
-                if(ch.find(s[i])!=ch.end())count++;
-                if(count){
-                    if(turn&1 ){
-                        if(count&1){
-                            turn++; 
-                            ans = true;
-                            i++;
-                            break;
-                        }
-                    }else{
-                        if(!count&1){
-                            turn++;
-                            ans = false;
-                            i++;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return ans;
         
+        unordered_set<char>ch={'a','e','i','o','u'};
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(ch.find(s[i])!=ch.end())return true;
+        }
+        return false;
     }
 };
