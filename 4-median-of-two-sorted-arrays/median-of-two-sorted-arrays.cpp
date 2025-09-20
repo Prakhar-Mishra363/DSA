ea684fb23521f358fe87896ccd17bac79e782d8a
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-09-20 19:01:27 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (80.44%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,61 +1,26 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size()+nums2.size();
-        if(n==1){
-            if(!nums1.size())return nums2[0];
-            else return nums1[0];
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n1 > n2)return findMedianSortedArrays(nums2,nums1);
+        int left = (n1 + n2 + 1)/2;
+        int low = 0, high = n1;
+        while(low <= high){
+            int mid1 = (low + high)/2;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1<n1) r1 = nums1[mid1];
+            if(mid2<n2) r2 = nums2[mid2];
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2 -1 >= 0) l2 = nums2[mid2-1];
+            if(l1 <= r2 && l2 <= r1){
+                if((n1+n2) & 1)return max(l1,l2);
+                return (float(max(l1,l2)+min(r1,r2)))/2.0;
+            }else if(l1> r2) high = mid1-1;
+            else low = mid1+1;
         }
-        int n1 = n/2-1;
-        int i=0,j=0;
-        int count = -1;
-        int ele1 = -1, ele2 = -1;
-        bool flag = false;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                count++;
-                if(count==n1){
-                    ele1 = nums1[i];
-                    flag = true;
-                    i++;break;
-                }
-                i++;
-            }else{
-                count++;
-                if(count==n1){
-                    ele1=nums2[j];
-                    flag = true;
-                    j++;break;
-                }
-                j++;
-            }
-        }
-        while(i<nums1.size() && !flag){
-            count++;
-            if(count == n1){
-                ele1 = nums1[i];
-                flag = true;
-                i++;break;
-            }
-            i++;
-        }
-        while(j<nums2.size() && !flag){
-            count++;
-            if(count == n1){
-                ele1 = nums2[j];
-                flag = true;
-                j++;
-                break;
-            }
-            j++;
-        }
-        if(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j])ele2 = nums1[i];
-            else ele2 = nums2[j];
-        }else if(i<nums1.size())ele2 = nums1[i];
-        else ele2 = nums2[j];
-
-        if(n & 1)return ele2;
-        return ((float(ele1)+float(ele2))/2);
+        return 0.0;
     }
 };
