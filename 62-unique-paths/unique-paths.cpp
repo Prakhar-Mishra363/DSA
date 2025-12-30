5523c82d5bac42434aaec2312510d06f4d113599
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-12-30 16:17:40 +0530

    Time: 15 ms (0.87%) | Memory: 9.6 MB (27.35%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     int uniquePathsHelper(int row, int col,int lastRow,int lastCol,vector<vector<int>>&dp){
+        //we reached the destination
         if(row == lastRow && col == lastCol)return 1;
-        if(row == lastRow) return uniquePathsHelper(row,col+1,lastRow,lastCol,dp);
-        if(col == lastCol) return uniquePathsHelper(row+1,col,lastRow,lastCol,dp);
+        //we are at the last row so the only available option is going right
+        if(row == lastRow) return dp[row][col] = uniquePathsHelper(row,col+1,lastRow,lastCol,dp);
+        //we are at the last col so only available option is going down
+        if(col == lastCol) return dp[row][col] = uniquePathsHelper(row+1,col,lastRow,lastCol,dp);
         if(dp[row][col] != -1)return dp[row][col];
+        //going to the right
         int right = uniquePathsHelper(row,col+1,lastRow,lastCol,dp);
+        //going down
         int down = uniquePathsHelper(row+1,col,lastRow,lastCol,dp);
         return dp[row][col] = (right + down);
     }
     int uniquePaths(int m, int n) {
+        //memoization
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return uniquePathsHelper(0,0,m-1,n-1,dp);
+        int result = uniquePathsHelper(0,0,m-1,n-1,dp);
+        for(auto a : dp){
+            for(auto b : a){
+                cout<<b<<" ";
+            }cout<<endl;
+        }
+        return result;
     }
 };
