a4057194a5342a175e4cd1b2e883829202679a3b
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2025-12-30 16:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (27.35%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //recursion + memoization
     int uniquePathsHelper(int row, int col,int lastRow,int lastCol,vector<vector<int>>&dp){
         //we reached the destination
         if(row == lastRow && col == lastCol)return 1;
@@ -17,12 +18,21 @@ public:
     int uniquePaths(int m, int n) {
         //memoization
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        int result = uniquePathsHelper(0,0,m-1,n-1,dp);
-        for(auto a : dp){
-            for(auto b : a){
-                cout<<b<<" ";
-            }cout<<endl;
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=1;
         }
-        return result;
+        for(int i=0;i<m;i++){
+            dp[i][n-1] = 1;
+        }
+        for(int row = m-2; row >= 0; row--){
+            for(int col = n-2; col >= 0; col--){
+                dp[row][col] = dp[row+1][col] + dp[row][col+1];
+            }
+        }
+        return dp[0][0];
+        // int result = uniquePathsHelper(0,0,m-1,n-1,dp);
+
+        
+        // return result;
     }
 };
