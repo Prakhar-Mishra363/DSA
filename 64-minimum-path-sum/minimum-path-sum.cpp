dd945b46791fd094b14949de67c3664b4dd71752
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-01 01:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (73.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -14,18 +14,35 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
-        // dp[m-1][n-1] = grid[m-1][n-1];
-
-        for(int currRow = m-1; currRow>=0; currRow--){
+        /* -> tabulation
+        // vector<vector<int>>dp(grid.size(),vector<int>(grid[0].size(),-1));
+        // for(int currRow = m-1; currRow>=0; currRow--){
+        //     for(int currCol = n-1; currCol>=0; currCol--){
+        //         if(currRow == grid.size()-1 && currCol == grid[0].size()-1)dp[currRow][currCol] = grid[currRow][currCol];
+        //         else if(currRow+1 == m)dp[currRow][currCol] = grid[currRow][currCol] + dp[currRow][currCol+1];
+        //         else if(currCol+1 == n)dp[currRow][currCol] = grid[currRow][currCol] + dp[currRow+1][currCol];
+        //         else dp[currRow][currCol] = grid[currRow][currCol] + min(dp[currRow][currCol+1],dp[currRow+1][currCol]);
+        //     }
+        // }
+        // return dp[0][0];
+        */
+        vector<int>dp(n,0);
+        for(int currRow = m-1;currRow>=0;currRow--){
+            vector<int>temp(n,0);
             for(int currCol = n-1; currCol>=0; currCol--){
-                if(currRow == grid.size()-1 && currCol == grid[0].size()-1)dp[currRow][currCol] = grid[currRow][currCol];
-                else if(currRow+1 == m)dp[currRow][currCol] = grid[currRow][currCol] + dp[currRow][currCol+1];
-                else if(currCol+1 == n)dp[currRow][currCol] = grid[currRow][currCol] + dp[currRow+1][currCol];
-                else dp[currRow][currCol] = grid[currRow][currCol] + min(dp[currRow][currCol+1],dp[currRow+1][currCol]);
+                if(currRow == grid.size()-1 && currCol == grid[0].size()-1)temp[currCol] = grid[currRow][currCol];
+                else if(currRow == grid.size()-1){
+                    temp[currCol] = grid[currRow][currCol] + temp[currCol+1];
+                }else if(currCol == n-1){
+                    temp[currCol] = grid[currRow][currCol] + dp[currCol];
+                }
+                else{
+                    temp[currCol] = grid[currRow][currCol] + min(temp[currCol+1],dp[currCol]);
+                }
             }
+            dp = temp;
         }
-        return dp[0][0];
+        return dp[0];
         // int result = minPathSumHelper(0,0,grid,dp);
         // for(auto a:dp){
         //     for(auto b:a){
