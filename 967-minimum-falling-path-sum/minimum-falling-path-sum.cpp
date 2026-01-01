be1c59b3bf12e996c666dd0989e2352fa3b94a9f
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-01 16:44:43 +0530

    Time: 1 ms (44.26%) | Memory: 15.1 MB (38.56%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -11,7 +11,8 @@ public:
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int minFallingPath = INT_MAX;
-        vector<vector<int>>dp(matrix.size(),vector<int>(matrix.size(),-1));
+        vector<int>dp(matrix.size()); 
+        dp = matrix[matrix.size()-1];
         /* -> recursion + memoization
         for(int col=0;col<matrix.size();col++){
             int tempResult = minFallingPathSumHelper(0,col,matrix,dp);
@@ -19,24 +20,19 @@ public:
         }
         return minFallingPath;
         */
-        for(int i=0;i<matrix.size();i++){
-            dp[matrix.size()-1][i] = matrix[matrix.size()-1][i];
-        }
-        // int result = INT_MAX;
         for(int currRow = matrix.size()-2; currRow>=0; currRow--){
+            vector<int>temp(matrix.size(),0);
             for(int currCol = 0; currCol < matrix.size(); currCol++){
-                int leftDiaCell = (currCol == 0) ? 1e5 : dp[currRow+1][currCol-1] + matrix[currRow][currCol];
-                int downBelowCell = dp[currRow+1][currCol] + matrix[currRow][currCol];
-                int rightDiaCell = (currCol == matrix.size()-1) ? 1e5 : dp[currRow+1][currCol+1] + matrix[currRow][currCol];
-                dp[currRow][currCol] = min(leftDiaCell,min(downBelowCell,rightDiaCell));
-                // result = min(result,dp[currRow,currCol]);
+                int leftDiaCell = (currCol == 0) ? 1e5 : dp[currCol-1];
+                int downBelowCell = dp[currCol];
+                int rightDiaCell = (currCol == matrix.size()-1) ? 1e5 : dp[currCol+1];
+                temp[currCol] = min(leftDiaCell,min(downBelowCell,rightDiaCell)) + matrix[currRow][currCol];
             }
+            dp = temp;
         }
         for(auto a:dp){
-            for(auto b:a){
-                cout<<b<<" ";
-            }cout<<endl;
+            cout<<a<<" ";
         }
-        return *min_element(dp[0].begin(),dp[0].end());
+        return *min_element(dp.begin(),dp.end());
     }
 };
