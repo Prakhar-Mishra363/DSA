d0f3d6d7712a12df40202832c33d0814c55a77b3
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-02 16:26:16 +0530

    Time: 1776 ms (18.65%) | Memory: 35.8 MB (81.12%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
+    // void subarraySumHelper(int idx,int k,int currSum ,int &count,vector<int>&nums){
+    //     if(idx == nums.size())
+    // }
     int subarraySum(vector<int>& nums, int k) {
-        if(nums.size()==0)return 0;
-        if(nums.size()==1){
-            if(nums[0]==k)return 1;
-            else return 0;
-        }
-        int count=0;
-        vector<int>preSum(nums.size(),0);
-        unordered_map<int,int>m;
-        preSum[0]=nums[0];
-        for(int i=1;i<nums.size();i++){
-            // prefixSum+=nums[i];
-            // preSum.push_back(preSum[i-1]+nums[i]);
-            preSum[i]=preSum[i-1]+nums[i];
-            
-        }
+        vector<int>prefixSum(nums.size(),0);
+        int count = 0;
         for(int i=0;i<nums.size();i++){
-            if(preSum[i]==k){
-                count++;
-                // m[k]=1;
-                // continue;
-            }
-            if(m.find(preSum[i]-k)!=m.end()){
-                // m[preSum[i]]++;
-                count+=m[preSum[i]-k];
-                m[preSum[i]]++;
-                continue;
-            }
-            if(m.find(preSum[i])!=m.end()){
-                m[preSum[i]]++;
-            }else{
-                m[preSum[i]]=1;
+            int sum = nums[i];
+            if(sum == k)count++;
+            for(int j=i+1;j<nums.size();j++){
+                sum += nums[j];
+                if(sum == k)count++;
             }
         }
-
         return count;
     }
 };
