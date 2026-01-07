20e5c213eafc1d11d2ba12d67ca30f44466e2f03
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-07 23:53:01 +0530

    Time: 47 ms (23.15%) | Memory: 61.4 MB (13.32%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int changeHelper(int idx, int currAmount, const vector<int> &coins, vector<vector<int>> &dp){
+        // if(currAmount == 0)return dp[idx][currAmount] = 1;
+        if(idx == coins.size()-1){
+            if((currAmount % coins[idx]) == 0)return dp[idx][currAmount] = 1;
+            else return dp[idx][currAmount] = 0;
+        }
+        if(dp[idx][currAmount] != -1)return dp[idx][currAmount];
+        int picked = (currAmount >= coins[idx]) ? changeHelper(idx ,currAmount - coins[idx], coins, dp ) : 0 ;
+        int left = changeHelper(idx + 1,currAmount , coins, dp);
+        return dp[idx][currAmount] = (picked + left);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        
+        /*
+        // -> memoization + recursion 
+        vector<vector<int>>dp(n , vector<int>(amount+1,-1));
+        int result = changeHelper(0,amount,coins,dp);
+        for(auto a : dp){
+            for(auto b : a){
+                cout<<b<<" ";
+            }cout<<endl;
+        }
+        return result;
+        */
+        //Tabulation
+        
+        
+        vector<vector<unsigned long long>>dp(n,vector<unsigned long long>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            dp[n-1][i] = (i % coins[n-1] == 0) ? 1 : 0;
+        }
+        // if(coins[n-1] <= amount ){
+        //     if(amount % coins[n-1] == 0)dp[n-1][amount] = 1;
+        //     dp[n-1][coins[n-1]] = 1;
+        // }
+        // else dp[n-1][coins] = 1;
+        for(int idx=n-2; idx>= 0; idx--){
+            for(int currAmount=0; currAmount<= amount; currAmount++){
+                unsigned long long picked = (currAmount >= coins[idx]) ? dp[idx][currAmount - coins[idx]] : 0 ;
+                unsigned long long left = dp[idx + 1][currAmount];
+                dp[idx][currAmount] = (picked + left);
+            }
+        }
+        // for(auto a : dp){
+        //     for(auto b : a){
+        //         cout<<b<<" ";
+        //     }cout<<endl;
+        // }
+        return dp[0][amount];
+        
+    }
+};
