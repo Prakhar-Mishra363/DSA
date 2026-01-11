fbc3abf5acd34c40b42cea160b42d314a7a5cbeb
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-11 17:54:37 +0530

    Time: 1508 ms (28.98%) | Memory: 9.3 MB (83.71%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -23,8 +23,24 @@ public:
         return dp[currIdx] = minCost;
     }
     int minCut(string s) {
+        /* (recursion + memoization)
         vector<int>dp(s.size() + 1 , -1);
         int result = minCutHelper(0 ,  s , dp) - 1;
         return result;
+        */
+        //Tabulation
+        int n = s.size();
+        vector<int>dp(n+1 , 0);
+        for(int idx = n-1 ; idx>=0 ; idx--){
+            int minCost = INT_MAX;
+            for(int j = idx ; j<n ; j++){
+                if(isPalindrom(idx , j , s)){
+                    int cost = 1 + dp[j+1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[idx] = minCost;
+        }
+        return dp[0]-1;
     }
 };
