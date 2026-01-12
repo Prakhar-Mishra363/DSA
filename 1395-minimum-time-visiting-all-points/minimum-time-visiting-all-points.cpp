5916086f8bf023461a8dbe6a215502f249746df7
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-12 19:13:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (61.26%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -7,22 +7,7 @@ public:
         int n = points.size();
         int time = 0;
         for(int currRow = 0 ; currRow<n-1 ; currRow++){
-                if(abscissa == points[currRow + 1][0] && ordinate != points[currRow + 1][1]){
-                    time += abs(ordinate - points[currRow + 1][1]);
-                    ordinate = points[currRow + 1][1];
-                    continue;
-                }else if(abscissa != points[currRow + 1][0] && ordinate == points[currRow + 1][1]){
-                    time += abs(abscissa - points[currRow + 1][0]);
-                    abscissa = points[currRow + 1][0];
-                    continue;
-                }
-                while(((abscissa != points[currRow + 1][0] || ordinate != points[currRow + 1][1])) && currRow < n ){
-                    if(abscissa > points[currRow + 1][0])abscissa--;
-                    else if(abscissa < points[currRow + 1][0])abscissa++;
-                    if(ordinate > points[currRow + 1][1])ordinate--;
-                    else if(ordinate < points[currRow + 1][1])ordinate++;
-                    time++;
-                }
+            time += max(abs(points[currRow][0] - points[currRow + 1][0]) , abs(points[currRow][1] - points[currRow + 1][1]));
         }
         return time;
     }
