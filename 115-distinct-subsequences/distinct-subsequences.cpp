6712a3e8cf8e33ecdb33cdffe469313c19f593a5
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-13 17:54:07 +0530

    Time: 27 ms (68.66%) | Memory: 44.3 MB (20.51%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int numDistinctHelper(int idx1, int idx2 , string &s , string &t , vector<vector<int>> &dp){
-        if(idx2 == t.size())return 1;
-        if(idx1 == s.size())return 0;
+        if(idx2 == t.size())return dp[idx1][idx2] = 1;
+        if(idx1 == s.size())return dp[idx1][idx2] = 0;
         if(dp[idx1][idx2] != -1)return dp[idx1][idx2];
         if(s[idx1] == t[idx2]){
             int goingAhead = numDistinctHelper(idx1 + 1 , idx2 + 1 , s ,t , dp) ;
@@ -12,7 +12,31 @@ public:
         return dp[idx1][idx2] = numDistinctHelper(idx1 + 1 , idx2 , s ,t , dp);
     }
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.size() , vector<int>(t.size() , -1));
-        return numDistinctHelper(0 , 0 , s , t , dp);
+        /*
+        //(recursion + memoization)
+        vector<vector<int>>dp(s.size() + 1 , vector<int>(t.size() + 1 , -1));
+        int result = numDistinctHelper(0 , 0 , s , t , dp);
+        for(const auto &a : dp){
+            for(const auto &b : a){
+                cout<<b<<" ";
+            }cout<<endl;
+        }
+        return result;
+        */
+        //tabulation
+        vector<vector<unsigned long long>>dp(s.size() + 1 , vector<unsigned long long>(t.size() + 1 , 0));
+        for(int i=0; i<=s.size() ; i++){
+            dp[i][t.size()] = 1;
+        }
+        for(int idx1=s.size()-1 ; idx1>=0 ; idx1--){
+            for(int idx2=t.size()-1 ; idx2>=0 ; idx2--){
+                if(s[idx1] == t[idx2]){
+                    dp[idx1][idx2] = (dp[idx1 + 1][idx2 + 1] + dp[idx1 + 1][idx2]); 
+                }else{
+                    dp[idx1][idx2] = dp[idx1 + 1][idx2];
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
