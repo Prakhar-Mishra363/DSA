c28e1b769ff19532389e0b1b99762ebbeabeade4
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-13 21:55:38 +0530

    Time: 6 ms (69.00%) | Memory: 13.1 MB (73.10%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minDistanceHelper(int idx1 , int idx2 , const string &word1 , const string &word2 , vector<vector<int>> &dp){
-        if(idx1 == word1.size())return (word2.substr(idx2)).size();
-        if(idx2 == word2.size())return (word1.substr(idx1)).size();
+        if(idx1 == word1.size())return dp[idx1][idx2] = (word2.substr(idx2)).size();
+        if(idx2 == word2.size())return dp[idx1][idx2] = (word1.substr(idx1)).size();
         if(dp[idx1][idx2] != -1)return dp[idx1][idx2];
         if(word1[idx1] == word2[idx2]){
             return dp[idx1][idx2] = minDistanceHelper(idx1 + 1 , idx2 + 1 , word1 , word2 , dp);
@@ -13,7 +13,35 @@ public:
         return dp[idx1][idx2] =  1 + min(replaced , min(inserted , erased));
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size() , vector<int>(word2.size() , -1));
-        return minDistanceHelper(0 , 0 , word1 , word2 , dp);
+        /* (recursion + memoization)
+        vector<vector<int>>dp(word1.size() + 1 , vector<int>(word2.size() + 1 , -1));
+        int result = minDistanceHelper(0 , 0 , word1 , word2 , dp);
+        for(auto &a : dp){
+            for(auto &b : a){
+                cout<<b<<" ";
+            }cout<<endl;
+        }
+        return result;
+        */
+        vector<vector<int>>dp(word1.size() + 1 , vector<int>(vector<int>(word2.size() + 1 , 0)));
+        for(int i=0;i<=word1.size();i++){
+            dp[i][word2.size()] = word1.size()-i;
+        }
+        for(int i=0; i<=word2.size() ; i++){
+            dp[word1.size()][i] = word2.size()-i;
+        }
+        for(int idx1 = word1.size()-1 ; idx1>= 0 ; idx1--){
+            for(int idx2=word2.size()-1 ; idx2>=0 ; idx2--){
+                if(word1[idx1] == word2[idx2]){
+                    dp[idx1][idx2] = dp[idx1 + 1][idx2 + 1];
+                }else{
+                    int replaced = dp[idx1 + 1][idx2 + 1];
+                    int inserted = dp[idx1][idx2 + 1];
+                    int erased = dp[idx1 + 1][idx2];
+                    dp[idx1][idx2] =  1 + min(replaced , min(inserted , erased));
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
