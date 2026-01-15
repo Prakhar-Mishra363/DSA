16bb382b6646526f8a546d9deba91976277a872b
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-15 18:59:26 +0530

    Time: 5 ms (20.79%) | Memory: 22 MB (13.83%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int maxProfitHelper(int idx , bool hasBought , const vector<int> &prices , vector<vector<int>> &dp){
+        //base case -> we reached the last day
+        if(idx == prices.size()-1){
+            //if boughtStockPrice == 0 => means we need to buy the stock so we simply return 0
+            if(hasBought)return prices[idx];
+            /*
+            we have some stock to sell, so if boughtStockPrice>prices[idx] we 
+            return the profit as 0 => we'll get loss if we sell,
+            else simply return the profit
+            */
+            return 0;
+        }
+        //dp memoization
+        if(dp[idx][hasBought] != -1)return dp[idx][hasBought];
+        //if current bought stock's value is greater than last days stocks value
+        //here boughtStockPrice>0 so we have bought.
+        if(hasBought ){
+            //case 1 -> we try to sell it on another day regardless of today's stock price
+            int sellOnAnotherDay = maxProfitHelper(idx + 1 , hasBought , prices , dp);
+            //case 2nd -> if today's stock price is greater than boughtStockPrice then we sell it today and try to buy the stock another day else 0
+            int sellingStock = prices[idx] + maxProfitHelper(idx + 1 , false , prices , dp) ;
+            return dp[idx][hasBought] = max(sellOnAnotherDay , sellingStock);
+        }
+        //here boughtStockPrice = 0 , we buy the stock on that day and try to sell it on the upcoming days
+        int stockIsBought = maxProfitHelper(idx + 1 , true , prices , dp) - prices[idx];
+        //case 3 -> we buy today's stock discarding the current bought stock to get the profit
+        int doNotBuy = maxProfitHelper(idx + 1 , false , prices , dp);
+        return dp[idx][hasBought] = max(stockIsBought , doNotBuy);
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int maxColumn = *max_element(prices.begin() , prices.end());
+        //recursion + memoization
+        vector<vector<int>>dp(prices.size() + 1 , vector<int>(2 , -1));
+        return maxProfitHelper(0 , false, prices , dp);
+        //tabulation
+        // int n = prices.size();
+        // vector<vector<int>> dp(prices.size() , vector<int>(maxColumn+1 , 0));
+        // vector<int>dp(maxColumn+1 , 0) , tmp(maxColumn+1 , 0);
+        //base case
+        // for(int i=1 ; i<=maxColumn ; i++){//i can't be zero as having 0 stock means u n
+        //     dp[i] = (prices[n-1] > i) ? prices[n-1] - i : 0;
+        // }
+        // for(int idx = n-2 ; idx>=0 ; idx--){
+        //     for(int boughtStockPrice=0 ; boughtStockPrice<=maxColumn ; boughtStockPrice++){
+        //         if(boughtStockPrice > 0){
+        //             int sellOnAnotherDay = dp[boughtStockPrice];
+        //             int sellingStock = (prices[idx] > boughtStockPrice) ? prices[idx] - boughtStockPrice + dp[0] : 0;
+        //             tmp[boughtStockPrice] = max(sellOnAnotherDay , sellingStock);
+        //         }else{
+        //             int stockIsBought = dp[prices[idx]];
+        //             int doNotBuy = dp[0];
+        //             tmp[boughtStockPrice] = max(stockIsBought , doNotBuy);
+        //         }
+        //     }
+        //     dp = tmp;
+        // }
+        // return dp[0];
+
+    }
+};
