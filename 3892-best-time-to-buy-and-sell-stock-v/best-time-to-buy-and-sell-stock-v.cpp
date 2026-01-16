5f66fdb3fc896772a7cb959ca56d11b0079af9aa
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-16 19:52:42 +0530

    Time: 1547 ms (5.05%) | Memory: 560.3 MB (5.23%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -36,9 +36,57 @@ public:
     }
     //requires a state for indicating normal transaction and short selling transaction
     long long maximumProfit(vector<int>& prices, int k) {
+        /*
+        //(recusion + memoization)
         int day = 0;
         int transactions = 2 * k;
         vector<vector<vector<long long>>>dp(prices.size() , vector<vector<long long>>(2*k + 1 , vector<long long>(3 , -1)));
         return maximumProfitHelper(day , transactions , 0 , prices , dp);
+        */
+        //tabulation
+        int n = prices.size();
+        vector<vector<vector<long long>>>dp(prices.size() + 1, vector<vector<long long>>(2*k + 1 , vector<long long>(3 , 0)));
+        //base cases
+        //for day = n 
+        for(int transaction=1 ; transaction<=2 * k ; transaction++){
+            dp[n][transaction][1] = -1e9;
+            dp[n][transaction][2] = -1e9;
+        }
+        //for transaction = 0 (if transactions made to be are 0 then for transactionType 1 and 2 the value returned is -1e9)
+        for(int day=0 ; day<=n ; day++){
+            dp[day][0][1] = -1e9;
+            dp[day][0][2] = -1e9;
+        }
+        for(int day=n-1 ; day>=0 ; day--){
+            for(int transactions=1 ; transactions<=2*k ; transactions++){
+                for(int transactionType=0 ; transactionType<3 ; transactionType++){
+                    if(transactionType){
+                        //normal transaction is being made
+                        if(transactionType == 1){
+                            //selling the stock and setting the transactionType to nothing to make normal or short selling again
+                            long long sellingToday = dp[day + 1][transactions-1][0] + prices[day];
+                            //selling any other day
+                            long long sellingAnotherDay = dp[day + 1][transactions][transactionType];
+                            dp[day][transactions][transactionType] = max(sellingToday , sellingAnotherDay);
+                        }else{
+                            //short selling transaction is being made
+                            long long buyingToday = dp[day + 1][transactions-1][0] - prices[day];
+                            //buying another day
+                            long long buyingAnotherDay = dp[day + 1][transactions][transactionType];
+                            dp[day][transactions][transactionType] = max(buyingToday , buyingAnotherDay);
+                        }
+                    }else{
+                        //making normal transaction
+                        long long buying = dp[day + 1][transactions-1][1] - prices[day];
+                        long long notBuying = dp[day + 1][transactions][transactionType];
+                        //making short selling transaction
+                        long long shortSelling = dp[day + 1][transactions-1][2] + prices[day];
+                        dp[day][transactions][transactionType] = max(max(buying ,notBuying) , shortSelling);
+                    }
+                }
+            }
+        }
+        return dp[0][2*k][0];
+
     }
 };
