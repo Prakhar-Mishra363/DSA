708d2d9f84a66cf8f3daf07fa72736eb7374bc21
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-17 00:07:13 +0530

    Time: 9 ms (72.17%) | Memory: 58.8 MB (96.69%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -18,21 +18,22 @@ public:
         vector<vector<int>>dp(prices.size() , vector<int>(2 , -1));
         return maxProfitHelper(0 , false , fee , prices , dp);
         */
-        vector<vector<int>>dp(prices.size()+1 , vector<int>(2 , 0));
+        vector<int>dp(2 , 0) , tmp(2 , 0);
         int n=prices.size();
         for(int day=n-1 ; day>=0 ; day--){
             for(int hasBought=1 ; hasBought>=0 ; hasBought--){
                 if(hasBought){
-                    int sellingToday = prices[day] - fee + dp[day + 1][0];
-                    int sellingAnotherDay = dp[day + 1][hasBought];
-                    dp[day][hasBought] = max(sellingToday , sellingAnotherDay);
+                    int sellingToday = prices[day] - fee + dp[0];
+                    int sellingAnotherDay = dp[hasBought];
+                    tmp[hasBought] = max(sellingToday , sellingAnotherDay);
                 }else{
-                    int buyingToday = dp[day + 1][1] - prices[day];
-                    int notBuying = dp[day + 1][hasBought];
-                    dp[day][hasBought] = max(buyingToday , notBuying);
+                    int buyingToday = dp[1] - prices[day];
+                    int notBuying = dp[hasBought];
+                    tmp[hasBought] = max(buyingToday , notBuying);
                 }
             }
+            dp = tmp;
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
