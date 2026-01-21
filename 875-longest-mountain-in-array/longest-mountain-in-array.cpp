32cf948053ac3552faed4347ec0f4c1c818d6516
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-21 22:03:26 +0530

    Time: 237 ms (5.01%) | Memory: 24.2 MB (6.74%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int longestMountainHelper(vector<int> &arr){
+        int n=arr.size();
+        vector<int>dp(n , 1);
+        vector<int>dpInc(n , 1);
+        vector<int>dpDec(n , 1);
+        int maxEle=0;
+        for(int idx=1 ; idx<=n-2 ; idx++){
+            int prev=idx-1;
+            int prevCount=0;
+            while(prev>=0 && arr[prev] < arr[prev+1]){
+                prevCount++;
+                prev--;
+            }
+            int nextIdx=idx+1;
+            int nextCount=0;
+            while(nextIdx < n && (arr[nextIdx] < arr[nextIdx-1])){
+                nextCount++;
+                nextIdx++;
+            }
+            dpInc[idx]=prevCount;
+            dpDec[idx]=nextCount;
+            if(dpInc[idx]>0 && dpDec[idx]>0){
+                maxEle =max(maxEle , dpInc[idx] + dpDec[idx] + 1);
+            }
+        }
+        return maxEle;
+
+    }
+    int longestMountain(vector<int>& arr) {
+        if(arr.size()<3)return 0;
+        return longestMountainHelper(arr);
+    }
+};
