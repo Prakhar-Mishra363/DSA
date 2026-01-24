09b76d5749ab120fb1c58ead483057a5fdc5e0be
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-01-24 22:41:41 +0530

    Time: 166 ms (76.78%) | Memory: 100 MB (30.18%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
-       priority_queue<int> max_heap;
-       priority_queue<int , vector<int> , greater<int>>min_heap;
-       for(int i=0; i<nums.size() ; i++){
-        max_heap.push(nums[i]);
-        min_heap.push(nums[i]);
+       sort(nums.begin(),nums.end());
+       int left=0 , right=nums.size()-1;
+       int minimized_max_pair_sum=0;
+       while(left<right){
+        minimized_max_pair_sum = max(minimized_max_pair_sum , nums[left++]+nums[right--]);
        }
-       int minimized_max_pair_sum=-1e9;
-       while(!max_heap.empty()){
-            int currSum=max_heap.top()+min_heap.top();
-            max_heap.pop();
-            min_heap.pop();
-            minimized_max_pair_sum = max(minimized_max_pair_sum , currSum);
-        }
-        return minimized_max_pair_sum;
+       return minimized_max_pair_sum;
     }
 };
