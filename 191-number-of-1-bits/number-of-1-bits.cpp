002195477b4210125898c3a1bd61c086e95b73a8
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-02-22 00:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.33%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        return __builtin_popcount(n);
+        int count=0;
+        while(n>=1){
+            count += (n&1 == 1) ? 1 : 0;
+            n = n>>1;
+        }
+        return count;
     }
 };
