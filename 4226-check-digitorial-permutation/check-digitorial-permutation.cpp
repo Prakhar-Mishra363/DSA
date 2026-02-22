3c5be160780c8d0be213784afaa0a4348a3f86bd
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-02-22 15:55:36 +0530

    Time: 3 ms (54.21%) | Memory: 8.2 MB (91.95%) - LeetSync

--- 4226-check-digitorial-permutation/check-digitorial-permutation.cpp
+++ 4226-check-digitorial-permutation/check-digitorial-permutation.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     bool isDigitorialPermutation(int n) {
-        vector<int>fact(10);
+        vector<int> fact(10) , freqOfOgDigits(10,0) , freqCalDigit(10,0);
         fact[0]=1;
-        int num=1;
+        int num=1 , digitorialNum=0;;
         while(num<=9){
             fact[num]=num*fact[num-1];
             num++;
         }
-        int digitorialNum=0;
-        vector<int>freqOfOgDigits(10,0);
         while(n){
             freqOfOgDigits[n%10]++;
             digitorialNum += fact[n%10];
             n /= 10;
         }
-        vector<int>freqCalDigit(10,0);
         while(digitorialNum){
             freqCalDigit[digitorialNum%10]++;
             digitorialNum /= 10;
