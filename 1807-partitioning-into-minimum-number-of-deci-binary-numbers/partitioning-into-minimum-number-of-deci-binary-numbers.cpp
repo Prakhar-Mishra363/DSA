81af26ade5bcee4882f58f5ddaed27eccbee75f8
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-03-01 17:41:50 +0530

    Time: 4 ms (28.60%) | Memory: 16.5 MB (53.96%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -4,6 +4,7 @@ public:
         int curr_max=-1e5;
         for(int idx=0 ; idx<n.size() ; idx++){
             curr_max = max(curr_max , n[idx]-'0');
+            if(curr_max==9)break;
         }
         return curr_max;
     }
