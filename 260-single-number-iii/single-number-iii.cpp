b5a778043e8d7c8dd90f3c06604c9bd7966bb567
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-03-04 15:08:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (96.38%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int>toReturn;
-        unordered_set<int>tne;
-        int result=nums[0];
+        long result=nums[0];
         for(int idx=1 ; idx<nums.size() ; idx++){
             result ^= nums[idx];
         }
-        int firstOne,secOne;
-        for(int oneEle=0 ; oneEle<nums.size()-1; oneEle++){
-            bool isExe=false;
-            for(int secEle=oneEle+1 ; secEle<nums.size(); secEle++){
-                if(tne.find(oneEle) != tne.end()){
-                    isExe=true;
-                    break;
-                }
-                if(nums[oneEle]==nums[secEle]){
-                    isExe=true;
-                    tne.insert(secEle);
-                    break;
-                }
-                else if((nums[oneEle]^nums[secEle]) == result){
-                    firstOne=nums[oneEle];
-                    secOne=nums[secEle];
-                }
-            }
-            if(!isExe )return {firstOne,secOne};
+        int firstBit = (result&(result-1)) ^ result;
+        int firstOne=0,secOne=0;
+        for(int idx=0 ; idx<nums.size() ; idx++){
+            if(firstBit & nums[idx]) firstOne ^=nums[idx];
+            else secOne ^= nums[idx];
         }
-        //will not be executed tho
-        return {-1,-1};
+        return {firstOne,secOne};
     }
 };
