86573a698253715aa89410464a9dba90332c48e3
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-03-09 17:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (97.03%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -4,9 +4,7 @@ public:
         int power=31;
         bool isPositive=true;
         long currDividend=labs(dividend) , currDivisor=labs(divisor);
-        if(dividend<0 && divisor>0 || dividend>=0 && divisor<0){
-            isPositive=false;
-        }
+        isPositive = (dividend<0) == (divisor<0);
         long answer=0;
         while(power>=0 && (currDividend < currDivisor * (1L<<power)))power--;
         while(power >= 0){
