3f4c3ebc1d19319f14493fc64cf0e2a09412f0aa
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-03-10 22:07:44 +0530

    Time: 170 ms (70.73%) | Memory: 29 MB (43.09%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -19,9 +19,26 @@ public:
         return dp[wasOne][remainingZeros][remainingOnes]  = ways;
     }
     int numberOfStableArrays(int zero, int one, int limit) {
-        vector<vector<vector<long long>>>dp(2 , vector<vector<long long>>(zero+1 , vector<long long>(one+1 , -1)));
-        long long zero_in_start = helper(zero , one , true , limit , dp);
-        long long one_in_start = helper(zero , one , false , limit , dp);
-        return (zero_in_start + one_in_start) % MOD;
+        vector<vector<vector<long long>>>dp(2 , vector<vector<long long>>(zero+1 , vector<long long>(one+1 , 0)));
+        // long long zero_in_start = helper(zero , one , true , limit , dp);
+        // long long one_in_start = helper(zero , one , false , limit , dp);
+        dp[0][0][0]=dp[1][0][0]=1;
+        for(int remainingZeros=0 ; remainingZeros<=zero ; remainingZeros++){
+            for(int remainingOnes=0 ; remainingOnes<=one ; remainingOnes++){
+                long long ways = 0;
+                if(!remainingZeros && !remainingOnes)continue;
+                for(int len=1 ; len<=min(remainingZeros,limit) ; len++){
+                    ways = (ways + dp[1][remainingZeros-len][remainingOnes])%MOD;
+                }
+                dp[0][remainingZeros][remainingOnes]=ways;
+                ways=0;
+                for(int ones=1 ; ones<=min(remainingOnes , limit) ; ones++){
+                    ways = ways + dp[0][remainingZeros][remainingOnes-ones];
+                    ways %= MOD;
+                }
+                dp[1][remainingZeros][remainingOnes]=ways;
+            }
+        }
+        return (dp[0][zero][one] + dp[1][zero][one]) % MOD;
     }
 };
