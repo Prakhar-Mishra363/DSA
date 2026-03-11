79ab136bcbda468b090cae7aa5e0597899232e71
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-03-11 17:16:11 +0530

    Time: 2 ms (55.21%) | Memory: 7.8 MB (68.55%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-        int place=0 , minimum_flips_req=0 , comp_num = max(a , max(b , c));
-        int curr_bit = 1<<place;
-        while(curr_bit <= comp_num){
-            if((curr_bit & c)){
-                if(!(curr_bit & a) && !(curr_bit & b))minimum_flips_req++;
-            }else{
-                if( (curr_bit & a) && (curr_bit & b))minimum_flips_req += 2;
-                else if( (curr_bit & a) || (curr_bit & b) )minimum_flips_req += 1;
-            }
-            place++;
-            curr_bit = 1<<place;
-        }
+        int xored=(a|b)^c , minimum_flips_req=0;
+        minimum_flips_req += __builtin_popcount(xored);
+        minimum_flips_req += __builtin_popcount(xored & (a&b));
         return minimum_flips_req;
     }
 };
