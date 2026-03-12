a2f487fcb87fef0c0695c36d0523754276f6d2c2
Prakhar-Mishra363 <154508732+Prakhar-Mishra363@users.noreply.github.com>
2026-03-12 15:19:28 +0530

    Time: 104 ms (62.95%) | Memory: 246.3 MB (50.76%) - LeetSync

--- 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.cpp
+++ 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.cpp
@@ -10,62 +10,59 @@ class DSU{
             if(parent[u]<0)return u;
             return parent[u] = find_parent(parent[u]);
         }
-        void union_by_size(int u , int v){
+        bool union_by_size(int u , int v){
             int par_u = find_parent(u);
             int par_v = find_parent(v);
-            if(par_u == par_v)return;
+            if(par_u == par_v)return true;
             if(parent[par_u]>parent[par_v])swap(par_u , par_v);
             parent[par_u] += parent[par_v];
             parent[par_v]=par_u;
             total_comp--;
+            return false;
         }
 };
-bool comp(vector<int> a , vector<int> b){
+bool comp(vector<int> &a , vector<int> &b){
     return a[2]> b[2];
 }
 class Solution {
 public:
     int maxStability(int n, vector<vector<int>>& edges, int k) {
-        // priority_queue<vector<int> , vector<vector<int>> , comp_for_max_heap>pq;
         vector<vector<int>>non_must_edges;
         DSU d(n);
-        vector<int>mas;
+        int ans=INT_MAX;
         vector<int>nmas;
-        // sort(edges.begin() , edges.end() , comp);
         int max_poss_stab=INT_MAX;
-        //building the st of the "must" be included edges
+        //O(1e5)
         for(int idx=0 ; idx<edges.size() ; idx++){
-            auto curr_edge=edges[idx];
+            auto &curr_edge=edges[idx];
             if(curr_edge[3]){
-                if(d.find_parent(curr_edge[0]) == d.find_parent(curr_edge[1]))return -1;
-                else{
-                    d.union_by_size(curr_edge[0] , curr_edge[1]);
+                if(d.union_by_size(curr_edge[0] , curr_edge[1]))return -1;
+                else{  
+                    ans=min(ans , curr_edge[2]);
                 }
-                mas.push_back(curr_edge[2]);
-                // max_poss_stab = min(max_poss_stab , curr_edge[2]);
             }else{
                 non_must_edges.push_back(curr_edge);
             }
         }
+        //O(5*1e5)
         if(non_must_edges.size()>0){
             sort(non_must_edges.begin() , non_must_edges.end() , comp);
         }
+        //O(1e5)
         for(int idx=0 ; idx<non_must_edges.size() ;  idx++){
-            auto curr_edge=non_must_edges[idx];
-            if(d.find_parent(curr_edge[0]) == d.find_parent(curr_edge[1]))continue;
+            auto &curr_edge=non_must_edges[idx];
+            if(d.union_by_size(curr_edge[0] , curr_edge[1]))continue;
             else{
-                d.union_by_size(curr_edge[0] , curr_edge[1]);
                 nmas.push_back(curr_edge[2]);
             }
         }
+        //O(1e5)
         for(int idx=nmas.size()-1 ; idx>=0 && k>0 ; idx--){
             nmas[idx] *= 2;
             k--;
         }
-        int ans=INT_MAX;
         if(d.total_comp>1)return -1;
-        if(nmas.size()>0)ans = *min_element(nmas.begin() , nmas.end());
-        if(mas.size()>0)ans = min(ans , *min_element(mas.begin() , mas.end()));
+        if(nmas.size()>0)ans = min(ans , *min_element(nmas.begin() , nmas.end()));
         return ans;
     }
 };
